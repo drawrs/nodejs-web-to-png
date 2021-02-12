654fc2a09af3d6dba04edc226010a84e462369cc
Rizal Hilman <rizal@udacoding.com>
2021-02-12 20:50:33 +0700

    coba dah

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
 const puppeteer = require('puppeteer');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'image/png' });
-    var q = url.parse(req.url, true).query;
-    var encodedUrl = q.encodedUrl;
 
-    var decoded = new Buffer.from("" + encodedUrl, 'base64').toString('ascii');
-    console.log(decoded);
-    (async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.setViewport({
-        width: 1500,
-        height: 1500,
-        deviceScaleFactor: 1,
-      });
-
-      await page.goto(decoded);
-      await page.screenshot({ path: 'example.png' });
-      await browser.close();
-      var img = fs.readFileSync('example.png');
-      res.end(img, 'binary');
-
-      // res.end();
-    })();
-
-}).listen(8080);
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("Hello coy");
+    res.end(response);
+    
+});
+server.listen();
