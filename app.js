99a8da6fddec08b4438ed75ae07be86e24b0ecfc
Rizal Hilman <rizal@udacoding.com>
2021-02-12 21:08:05 +0700

    test 5

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var http = require('http');
-const puppeteer = require('puppeteer');
+var express = require('express')
+var app = express();
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("Hello coy");
-    res.end(response);
-    
-});
-server.listen();
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
