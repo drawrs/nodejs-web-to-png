50d8487488f9d0e6e6fea99c88821f3eb9b16825
Rizal Hilman <rizal@udacoding.com>
2021-02-12 21:15:59 +0700

    setup encoded url pasring

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express')
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  var q = url.parse(request.url, true).query;
+  var encodedUrl = q.encodedUrl;
+
+  var decoded = new Buffer.from("" + encodedUrl, 'base64').toString('ascii');
+  console.log(decoded);
+  response.send('Hello World! ' + decoded)
 })
 
 app.listen(app.get('port'), function() {
