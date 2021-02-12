5095052c3aef7e863f9dfb497f2fb310e3d3782d
Rizal Hilman <rizal@udacoding.com>
2021-02-12 21:38:50 +0700

    adding puppeteer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var http = require('http')
 var url = require('url')
 var fs = require('fs')
+const puppeteer = require('puppeteer')
 
 var app = express();
 
@@ -14,11 +15,28 @@ app.get('/', function(request, response) {
 
   var decoded = new Buffer.from("" + encodedUrl, 'base64').toString('ascii');
   console.log(decoded);
-  // response.send('Hello World! ' + decoded)
+
   var currentPath = process.cwd();
   console.log(currentPath);
-  var filepath = currentPath + '/example.png'
-  response.sendFile(filepath);
+  
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1500,
+      height: 1500,
+      deviceScaleFactor: 1,
+    });
+
+    await page.goto(decoded);
+    await page.screenshot({ path: 'example2.png' });
+    await browser.close();
+    console.log("im here");
+
+    var filepath = currentPath + '/example2.png'
+    response.sendFile(filepath);
+
+  })();
 
 })
 
